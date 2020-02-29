09823cdc199216ef20d33ed5863b268083dd9628
Levente Székely <szekelylevente2000@gmail.com>
2020-02-29 13:22:30 +0100

    Add project files.

---
+++ EloadasProject/Eloadas.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EloadasProject
+{
+    public class Eloadas
+    {
+        private bool[,] foglalasok;
+
+        public Eloadas(int sorokSzama, int helyekSzama)
+        {
+            if(sorokSzama>0&&helyekSzama>0)
+            {
+                foglalasok = new bool[sorokSzama, helyekSzama];
+                for (int i = 0; i < sorokSzama; i++)
+                {
+                    for (int j = 0; j < helyekSzama; j++)
+                    {
+                        foglalasok[i, j] = false;
+                    }
+                }
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        public int SzabadHelyek()
+        {
+            int db = 0;
+            for (int i = 0; i < foglalasok.GetLength(0); i++)
+            {
+                for (int j = 0; j < foglalasok.GetLength(1); j++)
+                {
+                    if(!foglalasok[i,j])
+                    {
+                        db++;
+                    }
+                }
+            }
+            return db;
+        }
+
+        public bool Foglalas()
+        {
+            bool l = false;
+            int i = 0;
+            int j = 0;
+            while((i<foglalasok.GetLength(0))&&(!l))
+            {
+                while((j<foglalasok.GetLength(1))&&(!l))
+                {
+                    if(!foglalasok[i,j])
+                    {
+                        foglalasok[i, j] = true;
+                        l = true;
+                    }
+                    j++;
+                }
+                i++;
+            }
+            return l;
+        }
+
+        public bool Teli()
+        {
+            return SzabadHelyek() == 0;
+        }
+
+        public bool Foglalt(int sorSzam, int helySzam)
+        {
+            if(sorSzam>0&&helySzam>0)
+            {
+                if(foglalasok[sorSzam-1,helySzam-1])
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
---
+++ EloadasProject/Tests/EloadasTest.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EloadasProject.Tests
+{
+    [TestFixture]
+    class EloadasTest
+    {
+        [TestCase]
+        public void ujEloadasRossz()
+        {
+            Assert.Throws<ArgumentException>(() => { Eloadas e = new Eloadas(2, 0); });
+        }
+
+        [TestCase]
+        public void foglalas()
+        {
+            Eloadas e = new Eloadas(1, 1);
+            Assert.AreEqual(true, e.Foglalas());
+            Assert.AreEqual(false, e.Foglalas());
+        }
+
+        [TestCase]
+        public void szabadHelyekSzama()
+        {
+            Eloadas e = new Eloadas(3, 3);
+            e.Foglalas();
+            e.Foglalas();
+            Assert.AreEqual(7, e.SzabadHelyek());
+        }
+
+        [TestCase]
+        public void TelivanE()
+        {
+            Eloadas e = new Eloadas(1, 1);
+            e.Foglalas();
+            Assert.AreEqual(true, e.Teli());
+            Eloadas e2 = new Eloadas(1, 2);
+            e2.Foglalas();
+            Assert.AreEqual(false, e2.Teli());
+        }
+
+        [TestCase]
+        public void FoglaltE()
+        {
+            Eloadas e = new Eloadas(1, 2);
+            e.Foglalas();
+            Assert.AreEqual(true, e.Foglalt(1,1));
+            Assert.AreEqual(false, e.Foglalt(1, 2));
+        }
+
+        [TestCase]
+        public void FoglaltERossz()
+        {
+            Eloadas e = new Eloadas(1, 2);
+            e.Foglalas();
+            Assert.Throws<ArgumentException>(() => { e.Foglalt(0, 1); });
+        }
+    }
+}
